1dacf7c475cdc21c8fbf8f7161326ef052ba8e4d
lordclips <72330305+lordclips@users.noreply.github.com>
2020-10-26 15:35:57 -0400

    Some helpful little changes.

--- guessing_game.py
+++ guessing_game.py
@@ -1,39 +1,83 @@
-import random
-from random import randint
+# You don't need to import random
+# to import from random
 
+# import random
+from random import randint, choice
 
-# Initial numbers
+# Pre initialization is not required
+# in this instance for these specifically.
+# Keep them declared in the loop.
+
+# randomNumber = 0
+# wrongGuess = 0
+# wrongGuess1 = 0
 
 points = 0
-
 lives = 10
 
-
 # Name
-name=str(input("What is your name?\n"))
-print("Hello",name +"!\n")
+# Inputs dont need to be cast to strings. They
+# come that way by default. They also
+# implicitly add a newline.
+name=input("What is your name?")
+
+# Including two examples here for reference:
+# These are the two ways to do easier string 
+# formatting.
+
+# f-strings (preferred)
+print(f"Hello {name} !")
+
+# .format()
+# print("Hello {} !".format(name))
 
 while True:
-    randomNumber = randint(0, 100)
-    wrongGuess = randint(0, 100)
-    wrongGuess1 = randint(0, 100)
+    # Small trick for generating lists is to use
+    # something called a "List comprehension".
+    # It makes choosing which one is correct a little
+    # more difficult but there's another way around it.
     
-    numberlist = [randomNumber, wrongGuess, wrongGuess1]
+    # randomNumber = randint(0, 100)
+    # wrongGuess = randint(0, 100)
+    # wrongGuess1 = randint(0, 100)
+    
+    numberlist = [randint(0, 100) for _ in range(3)]
+    correct = choice(number_list)
+    
     random.shuffle(numberlist)
 
-    print(name + ", please guess a number from the following list.\n", numberlist)
-    answer=int(input())
+    # Using commas to print numerous variables is
+    # fine but not used all that often due to lack
+    # of control.
+    print(f"{name}, please guess a number from the following list: {numberList}")
+    
+    answer = int(input())
 
-    if (answer != randomNumber):
+    # Parentheses not needed (and not recommended
+    # unless required by your logic for reasons
+    # of precedence.
+    if answer != corect:
         print("Wrong, try again!")
-        lives = lives - 1
-        print("Lives remainin: " + str(lives)+"\n")
+        
+        # Very basic, not really error handling but
+        # "You did wrong, user" code.
+        if not answer in numberlist:
+            print("Make sure you are choosing a valid number from the list.")
+        
+        # This is the same as lives = lives - 1
+        # but easier shorthand.
+        lives -= 1
+        
+        print(f"Lives remaining: {lives}")
+        
         if lives == 0:
             break
         
     else:
         print("Correct!")
-        points = points + 1
-        print("Total points: " + str(points) + "\n")
+        
+        # Shorthand for points = points + 1
+        points += 1
+        print(f"Total points: {points}")
 
-print(f"You died with {points} points collected. Thank you for playing the game {name}.")
+print(f"Thank you for playing the game, {name}.")
